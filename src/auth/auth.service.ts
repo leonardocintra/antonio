7f74b45f21f06af7e8bb14fbf548601c1ebfcf80
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2022-12-17 19:06:32 -0300

    feat: validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { compare as bcryptCompare } from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UsersService) { }
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findByUsername(username);
+        const passwordMatch = await bcryptCompare(password, user.password);
+        if (user && passwordMatch) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,4 @@
 import { Controller } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
 
 @Controller('users')
 export class UsersController {}
