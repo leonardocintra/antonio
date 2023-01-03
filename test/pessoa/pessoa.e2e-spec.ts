1b313726092537731d2f9c1da583afe9c3ff486d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-01-03 19:58:11 -0300

    feat: test sobrenome

--- test/pessoa/pessoa.e2e-spec.ts
+++ test/pessoa/pessoa.e2e-spec.ts
@@ -248,6 +248,103 @@ describe('PessoaController (e2e)', () => {
     expect(response.body.message[0]).toEqual("nome must be shorter than or equal to 100 characters")
   });
 
+  it('/api/v1/pessoa (POST - 400) - Cadastrar Pessoa - Sobrenome not send', async () => {
+    const token = await fazerLogin();
+
+    const pessoa = {
+      nome: faker.name.lastName(),
+      cpfCnpj: Util.getRandomCPF(),
+      sexo: SexoEnum.FEMININO,
+      email: faker.internet.email(),
+      enderecos: [],
+    };
+
+    const response = await request(app.getHttpServer())
+      .post(BASE_PATH)
+      .set('Authorization', 'Bearer ' + token)
+      .send(pessoa);
+
+    expect(response.body).toBeDefined();
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+    expect(response.body.statusCode).toEqual(400)
+    expect(response.body.message).toHaveLength(2)
+    expect(response.body.message[0]).toEqual("sobrenome must be shorter than or equal to 100 characters")
+    expect(response.body.message[1]).toEqual("sobrenome should not be empty")
+  });
+
+  it('/api/v1/pessoa (POST - 400) - Cadastrar Pessoa - Sobrenome null', async () => {
+    const token = await fazerLogin();
+
+    const pessoa: CreatePessoaDto = {
+      sobrenome: null,
+      nome: faker.name.lastName(),
+      cpfCnpj: Util.getRandomCPF(),
+      sexo: SexoEnum.FEMININO,
+      email: faker.internet.email(),
+      enderecos: [],
+    };
+
+    const response = await request(app.getHttpServer())
+      .post(BASE_PATH)
+      .set('Authorization', 'Bearer ' + token)
+      .send(pessoa);
+
+    expect(response.body).toBeDefined();
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+    expect(response.body.statusCode).toEqual(400)
+    expect(response.body.message).toHaveLength(2)
+    expect(response.body.message[0]).toEqual("sobrenome must be shorter than or equal to 100 characters")
+    expect(response.body.message[1]).toEqual("sobrenome should not be empty")
+  });
+
+  it('/api/v1/pessoa (POST - 400) - Cadastrar Pessoa - Sobrenome empty', async () => {
+    const token = await fazerLogin();
+
+    const pessoa: CreatePessoaDto = {
+      sobrenome: '',
+      nome: faker.name.lastName(),
+      cpfCnpj: Util.getRandomCPF(),
+      sexo: SexoEnum.FEMININO,
+      email: faker.internet.email(),
+      enderecos: [],
+    };
+
+    const response = await request(app.getHttpServer())
+      .post(BASE_PATH)
+      .set('Authorization', 'Bearer ' + token)
+      .send(pessoa);
+
+    expect(response.body).toBeDefined();
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+    expect(response.body.statusCode).toEqual(400)
+    expect(response.body.message).toHaveLength(1)
+    expect(response.body.message[0]).toEqual("sobrenome should not be empty")
+  });
+
+  it('/api/v1/pessoa (POST - 400) - Cadastrar Pessoa - Sobrenome mais que 100 caracteres', async () => {
+    const token = await fazerLogin();
+
+    const pessoa: CreatePessoaDto = {
+      sobrenome: 'aaaaaaaaaaaaaaadddddddddddd kkkkkkkkkkkkkkkkkkkkkkk dddddddddddd oooooooooooooooo oooooooooooooooo ppppppppppppp ccccccccc',
+      nome: faker.name.lastName(),
+      cpfCnpj: Util.getRandomCPF(),
+      sexo: SexoEnum.FEMININO,
+      email: faker.internet.email(),
+      enderecos: [],
+    };
+
+    const response = await request(app.getHttpServer())
+      .post(BASE_PATH)
+      .set('Authorization', 'Bearer ' + token)
+      .send(pessoa);
+
+    expect(response.body).toBeDefined();
+    expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+    expect(response.body.statusCode).toEqual(400)
+    expect(response.body.message).toHaveLength(1)
+    expect(response.body.message[0]).toEqual("sobrenome must be shorter than or equal to 100 characters")
+  });
+
   it('/api/v1/pessoa (POST - 201) - Cadastrar Pessoa sem endereço', async () => {
     const token = await fazerLogin();
 
